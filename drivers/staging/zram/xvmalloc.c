8617258e3148b2a50c4a3173f8e5d51bdde88b25
pinkflozd <lk@varjanta.com>
2011-10-09 23:20:34 +0200

    zcache: page cache compression support Frequently accessed filesystem data is stored in memory to reduce access to (much) slower backing disks. Under memory pressure, these pages are freed and when needed again, they have to be read from disks again. When combined working set of all running application exceeds amount of physical RAM, we get extereme slowdown as reading a page from disk can take time in order of milliseconds.
    Memory compression increases effective memory size and allows more pages to
    stay in RAM. Since de/compressing memory pages is several orders of magnitude
    faster than disk I/O, this can provide signifant performance gains for many
    workloads. Also, with multi-cores becoming common, benefits of reduced disk I/O
    should easily outweigh the problem of increased CPU usage.
    
    It is implemented as a backend for cleancache_ops [1] which provides
    callbacks for events such as when a page is to be removed from the page cache
    and when it is required again. We use them to implement a 'second chance' cache
    for these evicted page cache pages by compressing and storing them in memory
    itself.
    
    We only keep pages that compress to PAGE_SIZE/2 or less. Compressed chunks are
    stored using xvmalloc memory allocator which is already being used by zram
    driver for the same purpose. Zero-filled pages are checked and no memory is
    allocated for them.
    
    A separate pool is created for each mount instance for a cleancache-aware
    filesystem. Each incoming page is identified with <pool_id, inode_no, index>
    where inode_no identifies file within the filesystem corresponding to pool_id
    and index is offset of the page within this inode. Within a pool, inodes are
    maintained in an rb-tree and each of its nodes points to a separate radix-tree
    which maintains list of pages within that inode.
    
    While compression reduces disk I/O, it also reduces the space available for
    normal (uncompressed) page cache. This can result in more frequent page cache
    reclaim and thus higher CPU overhead. Thus, it's important to maintain good hit
    rate for compressed cache or increased CPU overhead can nullify any other
    benefits. This requires adaptive (compressed) cache resizing and page
    replacement policies that can maintain optimal cache size and quickly reclaim
    unused compressed chunks. This work is yet to be done. However, in the current
    state, it allows manually resizing cache size using (per-pool) sysfs node
    'memlimit' which in turn frees any excess pages *sigh* randomly.
    
    Finally, it uses percpu stats and compression buffers to allow better
    performance on multi-cores. Still, there are known bottlenecks like a single
    xvmalloc mempool per zcache pool and few others. I will work on this when I
    start with profiling.

--- drivers/staging/zram/xvmalloc.c
+++ drivers/staging/zram/xvmalloc.c
@@ -14,6 +14,8 @@
 #define DEBUG
 #endif
 
+#include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/bitops.h>
 #include <linux/errno.h>
 #include <linux/highmem.h>
@@ -324,11 +326,13 @@ struct xv_pool *xv_create_pool(void)
 
 	return pool;
 }
+EXPORT_SYMBOL_GPL(xv_create_pool);
 
 void xv_destroy_pool(struct xv_pool *pool)
 {
 	kfree(pool);
 }
+EXPORT_SYMBOL_GPL(xv_destroy_pool);
 
 /**
  * xv_malloc - Allocate block of given size from pool.
@@ -417,6 +421,7 @@ int xv_malloc(struct xv_pool *pool, u32 size, struct page **page,
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(xv_malloc);
 
 /*
  * Free block identified with <page, offset>
@@ -493,6 +498,7 @@ void xv_free(struct xv_pool *pool, struct page *page, u32 offset)
 	put_ptr_atomic(page_start, KM_USER0);
 	spin_unlock(&pool->lock);
 }
+EXPORT_SYMBOL_GPL(xv_free);
 
 u32 xv_get_object_size(void *obj)
 {
@@ -501,6 +507,7 @@ u32 xv_get_object_size(void *obj)
 	blk = (struct block_header *)((char *)(obj) - XV_ALIGN);
 	return blk->size;
 }
+EXPORT_SYMBOL_GPL(xv_get_object_size);
 
 /*
  * Returns total memory used by allocator (userdata + metadata)
@@ -509,3 +516,4 @@ u64 xv_get_total_size_bytes(struct xv_pool *pool)
 {
 	return pool->total_pages << PAGE_SHIFT;
 }
+EXPORT_SYMBOL_GPL(xv_get_total_size_bytes);
